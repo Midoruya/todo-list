ebf48cfa0c29e7b77dff3de81b9eacaa9b2308c6
Midoriya <wladimi482002@yandex.ru>
2022-07-29 19:12:19 +0300

    add search all task and add task file

--- src/command_handler/command_handler.cpp
+++ src/command_handler/command_handler.cpp
@@ -2,8 +2,8 @@
 #include "../utilities.h"
 #include <iostream>
 #include <filesystem>
-#include <format>
 #include <fstream>
+#include <stdio.h>
 #include <ctime>
 
 void command_handler::handle_new_command()
@@ -16,18 +16,26 @@ void command_handler::handle_new_command()
 void command_handler::search_all_task()
 {
     std::filesystem::path current_directory = std::filesystem::current_path();
-    current_directory.append(current_directory.string() + "\\" + this->save_in_folder);
+    current_directory.append(current_directory.string() + "/" + this->save_in_folder);
     if (std::filesystem::exists(current_directory.string()) == false)
         std::filesystem::create_directory(current_directory.string());
-
     this->create_new_task("test", "first", "asdasdasdasdadasdasd");
     for (const auto &task : std::filesystem::directory_iterator(current_directory.string()))
     {
-        std::cout << task.path().string() << std::endl;
+        std::string filename = task.path().stem().string();
+        std::vector<std::string> splite_filename = utilities::splite_string(filename);
+        bool is_complete = splite_filename[3] == "no" ? false : true;
+        std::string task_name = splite_filename[0];
+        std::string task_tag = splite_filename[2];
+        std::string task_date = splite_filename[1];
+        std::string task_description = "";
+        std::fstream new_file(task.path());
+        std::getline(new_file, task_description);
+        task_construct new_task = task_construct(task_name, task_description, task_date, task_tag, is_complete);
+        this->task_list.push_back(new_task);
+        std::cout << task_description.c_str() << std::endl;
     }
 }
-
-// TODO: check this function on create file task.Because it don't create file in directory
 
 void command_handler::create_new_task(std::string name, std::string tag, std::string description)
 {
@@ -40,13 +48,13 @@ void command_handler::create_new_task(std::string name, std::string tag, std::st
     date_time_str.append(":");
     date_time_str.append(std::to_string(local_time->tm_mday));
     std::filesystem::path current_directory = std::filesystem::current_path();
-    current_directory.append(current_directory.string() + "\\" + this->save_in_folder + "\\");
+    current_directory.append(current_directory.string() + "/" + this->save_in_folder + "/");
     char new_filename[0x248];
     std::sprintf(new_filename, "%s %s %s %s%s", name.c_str(), date_time_str.c_str(), tag.c_str(), "no", this->file_format.c_str());
     current_directory.append(new_filename);
     std::ofstream new_file(current_directory);
     if (!new_file.is_open())
         std::cout << "[Error] open and write in file" << std::endl;
-    new_file << description;
+    new_file << description.c_str() << std::endl;
     new_file.close();
 }
--- src/command_handler/command_handler.h
+++ src/command_handler/command_handler.h
@@ -2,11 +2,30 @@
 #include <map>
 #include <string>
 
+struct task_construct
+{
+    task_construct(std::string name, std::string description, std::string date, std::string tag, bool is_complete)
+    {
+        this->name = name;
+        this->description = description;
+        this->date = date;
+        this->tag = tag;
+        this->is_complete = is_complete;
+    }
+
+    bool is_complete;
+    std::string name;
+    std::string tag;
+    std::string date;
+    std::string description;
+};
+
 class command_handler
 {
 private:
     std::string file_format = ".task";
     std::string save_in_folder = "task_list";
+    std::vector<task_construct> task_list;
 
 public:
     std::vector<std::string> command_argument;
